04031d157b5c103e40b1c8e8e0612267348b31c5
chefnn <k.n.nuankaew@gmail.com>
2022-02-04 11:07:17 +0700

    add dashboard comps

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => {
+  return <div></div>
+}
+
+export default Header
+
+const Wrapper = styled.div`
+  width: calc(100%-3rem);
+  padding: 1rem 1.5rem;
+  border-bottom: 1px solid #282b2f;
+  display: flex;
+  align-items: center;
+`
+const Title = styled.div`
+  font-size: 2rem;
+  font-weight: 600;
+  flex: 1;
+`
---
+++ pages/Dashboard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Dashboard = ({ address }) => {
+  return <h2>{address}</h2>
+}
+
+export default Dashboard
