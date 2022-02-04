54a95f196ab313a462ad0fe620173abe061e22c4
chefnn <k.n.nuankaew@gmail.com>
2022-02-04 11:35:45 +0700

    add header

--- components/Header.js
+++ components/Header.js
@@ -2,13 +2,25 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Header = () => {
-  return <div></div>
+  return (
+    <Wrapper>
+      <Title>Assets</Title>
+      <ButtonsContainer>
+        <Button style={{ backgroundColor: '#3773f5', color: '#000' }}>
+          Buy / Sell
+        </Button>
+        <Button style={{ backgroundColor: '#3773f5', color: '#000' }}>
+          Send / Receive
+        </Button>
+      </ButtonsContainer>
+    </Wrapper>
+  )
 }
 
 export default Header
 
 const Wrapper = styled.div`
-  width: calc(100%-3rem);
+  width: calc(100% - 3rem);
   padding: 1rem 1.5rem;
   border-bottom: 1px solid #282b2f;
   display: flex;
@@ -18,4 +30,18 @@ const Title = styled.div`
   font-size: 2rem;
   font-weight: 600;
   flex: 1;
+`
+const ButtonsContainer = styled.div`
+  display: flex;
+`
+const Button = styled.div`
+  border: 1px solid #282b2f;
+  padding: 0.8rem;
+  font-size: 1.3rem;
+  font-weight: 500;
+  border-radius: 0.4rem;
+  margin-right: 1rem;
+  &:hover {
+    cursor: pointer;
+  }
 `
--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import Header from '../components/Header'
 const Dashboard = ({ address }) => {
-  return <h2>{address}</h2>
+  return (
+    <Wrapper>
+      <MainContainer>
+        <Header />
+      </MainContainer>
+    </Wrapper>
+  )
 }
 
 export default Dashboard
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+`
+const MainContainer = styled.div`
+  flex: 1;
+`
