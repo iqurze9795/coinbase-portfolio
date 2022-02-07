3702c4c68431374e0ff23b5a04ac62f6d21d6392
chefnn <k.n.nuankaew@gmail.com>
2022-02-07 14:56:41 +0700

    add grap token

---
+++ components/BalanceChart.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import styled from 'styled-components'
+import dynamic from 'next/dynamic'
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
+import 'chart.js/auto'
+const options = {
+  grid: {
+    show: false,
+    padding: {
+      left: 0,
+      right: 0,
+    },
+  },
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    sparkline: {
+      enabled: true,
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: 'smooth',
+    width: 2.5,
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shadeIntensity: 0.9,
+      opacityFrom: 0.5,
+      opacityTo: 0.2,
+      stops: [0, 80, 100],
+    },
+  },
+  xaxis: {
+    type: 'numeric',
+    lines: {
+      show: false,
+    },
+    axisBorder: {
+      show: false,
+    },
+    labels: { show: false },
+  },
+  yaxis: {
+    show: false,
+  },
+  tooltip: {
+    x: { show: false },
+  },
+  theme: {
+    monochrome: {
+      enabled: true,
+      color: '#F29299',
+      shadeTo: 'light',
+      shadeIntensity: 0.65,
+    },
+  },
+}
+const series = [
+  {
+    name: 'Moonbeam',
+    data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+  },
+]
+const BalanceChart = () => {
+  return (
+    <Wrapper>
+      <Chart
+        options={options}
+        series={series}
+        type="area"
+        width="100%"
+        height={200}
+      />
+    </Wrapper>
+  )
+}
+
+export default BalanceChart
+
+const Wrapper = styled.div``
--- components/Portfolio.js
+++ components/Portfolio.js
@@ -3,11 +3,30 @@ import { BsThreeDotsVertical } from 'react-icons/bs'
 import styled from 'styled-components'
 import { coins } from '../static/coins'
 import Coin from './Coin'
-import ChainInfo from './ChainInfo'
+import BalanceChart from './BalanceChart'
+import MoonbeamIcon from '../assets/coin-icon/moonbeam.png'
+import { FiChevronsUp } from 'react-icons/fi'
+import Image from 'next/image'
 function Portfolio() {
   return (
     <Wrapper>
       <Content>
+        <Chart>
+          <div>
+            <Balance>
+              <BalanceTitle>
+                <Image width={50} height={50} src={MoonbeamIcon} />
+                <p style={{ paddingLeft: '10px' }}>Moonbeam (GLMR)</p>
+              </BalanceTitle>
+              <BalanceValue>{'$'} 18.98</BalanceValue>
+              <PercentChange>
+                +5$ <FiChevronsUp />
+              </PercentChange>
+            </Balance>
+          </div>
+          <BalanceChart />
+        </Chart>
+
         <PortfolioTable>
           <TableItem>
             <Title>Your Assets</Title>
@@ -50,15 +69,42 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
 `
-
 const Content = styled.div`
   width: 100%;
   /* max-height: 1000px; */
   padding: 2rem 1rem;
 `
+
 const Chart = styled.div`
-  border: 1px solid #282b2f;
+  border: 1px solid #272b2f;
   padding: 1rem 2rem;
+`
+
+const Balance = styled.div``
+
+const BalanceTitle = styled.div`
+  color: #8a919e;
+  font-size: 0.9rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+const BalanceValue = styled.div`
+  color: #11ef68;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: 2.5rem;
+  font-weight: 700;
+  /* margin: 0.5rem 0; */
+`
+const PercentChange = styled.div`
+  color: #ef1111;
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+  font-weight: 600;
+  justify-content: flex-end;
 `
 
 const PortfolioTable = styled.div`
